9dab8641a0810105303d383788c0f2146249a7f2
Elmallah <ahmedwaheedelmallah2@gmail.com>
2025-06-24 10:20:07 +0300

    update

---
+++ Back/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+// mangoose
+//   .connect(
+//     "mongodb+srv://ahmedwaheedelmallah2:66lRKTrkakQxI3XL@ieee.xkbhtzt.mongodb.net/IEE?retryWrites=true&w=majority&appName=IEEE"
+//   )
+//   .cath((err) => console.log(err));
+app.listen(5000, () => {
+  console.log("ahmed");
+});
+// mongodb+srv://ahmedwaheedelmallah2:66lRKTrkakQxI3XL@ieee.xkbhtzt.mongodb.net/?retryWrites=true&w=majority&appName=IEEE
